83a62444591ab63c237cd19d9be3602aa6131a5a
Prabhu Teja Malli <prabhutejamalli@gmail.com>
2024-08-22 16:25:34 +0530

    this is my 6th commint

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" /> 
-        <h1>Prabhu Teja Malli(Thanks) tejamlml</h1>
+        <h1>Prabhu Teja Malli(Thanks) tejamlm2</h1>
       </header>
     </div>
   );
